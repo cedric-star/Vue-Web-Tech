2e336a80dabfabb2a9064e047934238f4dd37638
cedric-star <cdrc.wnsch@gmail.com>
2024-12-04 23:40:38 +0100

    second commit, footer and header working

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,53 +1,25 @@
 <template>
-    <footer>
-        <div class="foot_items">
+    <footer class="foot_items">
             <p><b>Impressum</b></p>
             <p>©Cedric Wünsch</p>
-            <p><button @click="downloadFile">Documentation</button></p>
-        </div>
-        
-        
+            <p><a href="https://github.com/cedric-star/Vue-Web-Tech">Documentation</a></p>
     </footer>
 </template>
 <script>
 export default {
-    name: "Footer",
-    methods: {
-        downloadFile() {
-            const link = document.createElement('a');
-            link.href = "./logo.png"; // Ersetze durch den Pfad zu deiner Datei
-            link.download = "logo.png"; // Ersetze durch den gewünschten Dateinamen
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
-    }
+    name: "Footer"
+
 }
 </script>
 
 <style>
-#grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr); /* Change 3 to the number of columns you want */
-    gap: 10px; /* Space between grid items */
-    padding: 10px; /* Padding around the grid */
-}
-
-#grid img {
-    width: 100%; /* Makes the images responsive */
-    height: auto; /* Maintains aspect ratio */
-}
 
 body, html {
     margin: 0;
     padding: 0;
 
 }
-footer {
-    display: flex;
-    flex-direction: column;
-    min-height: 16px;
-}
+
 .foot_items {
     position: fixed;
     bottom: 0;
@@ -56,9 +28,11 @@ footer {
     background-color: antiquewhite;
     text-align: center;
 
-    padding: 0;
-    margin: 0;
-    padding: 8px;
+
     border-radius: 2px;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);    
+
 }
 </style>
