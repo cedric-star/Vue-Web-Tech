69284c023420179686ad760ed4a7c2af4ea56572
cedric-star <cdrc.wnsch@gmail.com>
2024-12-06 22:31:53 +0100

    df

---
+++ src/views/Baking.vue
@@ -0,0 +1,18 @@
+<template>
+    <NavigationBar/>
+    <CreateRecipe type="baking"/>
+    <Footer/>
+</template>
+<script>
+import NavigationBar from '../components/NavigationBar.vue';
+import Footer from '../components/Footer.vue';
+import CreateRecipe from '../components/CreateRecipe.vue';
+
+export default {
+    components: {
+        NavigationBar,
+        CreateRecipe,
+        Footer
+    }
+}
+</script>
